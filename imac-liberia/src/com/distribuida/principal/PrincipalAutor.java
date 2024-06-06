7a3469a257057039bc2356bb12cdd6c88cc04362
MATUTINO <MATUTINO@DESKTOP-AQPRS3B>
2024-06-06 10:11:29 -0500

    hola

---
+++ imac-liberia/src/com/distribuida/principal/PrincipalAutor.java
@@ -0,0 +1,25 @@
+package com.distribuida.principal;
+
+import java.util.List;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.distribuida.dao.AutorDAO;
+import com.distribuida.entities.Autor;
+
+public class PrincipalAutor {
+
+	public static  void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Patron IK Inversión de Control
+		ClassPathXmlApplicationContext context =new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		AutorDAO autorDAO =context.getBean("AutorDAOmpl",AutorDAO.class);
+		List<Autor> autor=autorDAO.findALL();
+		autor.forEach(item ->{
+			System.out.println(item.toString());
+		});
+		context.close();
+	}
+
+}
